de81b9d79fdd8b1979da7bce3c00d321d8197359
namdeptrai69 <tuannam2505@gmail.com>
2020-05-29 19:43:54 +0700

    bai tap

---
+++ PHP/Unit02/Baitap/admin/authors/author_add_action.php
@@ -0,0 +1,22 @@
+<?php 
+	require_once('../../connection.php');
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$status = $_POST['status'];
+
+	$query = "INSERT INTO authors(name,email,password,status) VALUES ('".$name."','".$email."','".$password."','".$status."')";
+
+	$status = $conn->query($query);
+
+	if ($status == true) {
+		setcookie('msg','Thêm mới thành công',time()+5);
+		header('Location: authors.php');
+	} else {
+		setcookie('msg','Thêm mới không thành công',time()+5);
+		header('Location: author_add.php');
+	}
+	
+
+?>
